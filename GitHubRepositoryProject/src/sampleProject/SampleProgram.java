69476d430654ba155c6d8ad8ea1ee8348269a348
Eshwar Chary <Eshwar Chary@192.168.0.5>
2020-08-28 16:45:39 +0530

    Commit Version1.0

--- GitHubRepositoryProject/src/sampleProject/SampleProgram.java
+++ GitHubRepositoryProject/src/sampleProject/SampleProgram.java
@@ -1,5 +1,6 @@
 package sampleProject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +11,7 @@ public class SampleProgram {
 		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\driver's\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();	
 		driver.get("https://www.guru99.com/take-screenshot-selenium-webdriver.html#3");
+		driver.findElement(By.id("")).sendKeys("");
 
 	}
 
